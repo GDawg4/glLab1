1e88d84ea4f07a94d5720d581ec6cc247aeb4163
Rodrigo Garoz <garoz.r4@gmail.com>
2020-07-27 21:16:02 -0600

    Added inner polygon

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-    int height = 500;
+    int height = 800;
     int width = 800;
     gl myGl(height, width);
     myGl.glViewPort(0, 0, width, height);
@@ -15,12 +15,14 @@ int main()
     vector<vector<int>> poly2 {{321, 335}, {288, 286}, {339, 251}, {374, 302}};
     vector<vector<int>> poly3 {{377, 249}, {411, 197}, {436, 249}};
     vector<vector<int>> poly4 {{413, 177}, {448, 159}, {502, 88} ,{553, 53} ,{535, 36} ,{676, 37} ,{660, 52}, {750, 145}, {761, 179}, {672, 192}, {659, 214}, {615, 214}, {632, 230}, {580, 230},{597, 215}, {552, 214}, {517, 144}, {466, 180}};
-    vector<vector<int>> poly5 {{377, 249}, {411, 320}, {436, 249}};
+    vector<vector<int>> poly5 {{682, 175}, {708, 120}, {735, 148}, {739, 170}};
 
     myGl.glPoly(poly1);
     myGl.glPoly(poly2);
     myGl.glPoly(poly3);
     myGl.glPoly(poly4);
+    myGl.glColor(0,0,0);
+    myGl.glPoly(poly5);
     //myGl.glObj("lego.obj");
     myGl.glFinish();
 }
